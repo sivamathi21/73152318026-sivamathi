973869d04c4442db2bbe2430ce2810a98c299036
sivamathi21 <sivamathimurugaiyan@gmail.com>
2025-07-19 15:36:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + sivamathi</h1>
+      <h1>Vite + sivamathi + 73152318026 </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
